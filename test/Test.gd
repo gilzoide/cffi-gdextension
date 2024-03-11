b0fb5848ca8ec180f6827bc6f5849b5e55f1d17e
gilzoide <gilzoide@gmail.com>
2024-03-11 19:35:03 -0300

    Add test scene with test script, enable plugin, update Godot to 4.2

---
+++ test/Test.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var lib = preload("res://native_plugin/native_plugin.ffilibrary").open()
+	print(lib)
+	var f = lib.define_function("get_answer", FFI["int"], [])
+	print(f)
