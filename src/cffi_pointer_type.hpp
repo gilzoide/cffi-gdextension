b21c0dcd4082104a17c2c4043fc17715649b1758
gilzoide <gilzoide@gmail.com>
2024-06-02 12:22:58 -0300

    Add some docs to CFFIPointerType

--- src/cffi_pointer_type.hpp
+++ src/cffi_pointer_type.hpp
@@ -7,15 +7,40 @@ using namespace godot;
 
 namespace cffi {
 
+/**
+ * `CFFIType` specialized for pointer types.
+ */
 class CFFIPointerType : public CFFIType {
 	GDCLASS(CFFIPointerType, CFFIType);
 public:
+	/**
+	 * Necessary to define a Godot class.
+	 * @warning Never use this constructor.
+	 */
 	CFFIPointerType();
+	/**
+	 * Create a pointer type that points to elements with type `element_type`.
+	 */
 	CFFIPointerType(Ref<CFFIType> element_type);
 
+	/**
+	 * Get the element type.
+	 */
 	Ref<CFFIType> get_element_type() const;
 
+	/**
+	 * Write a `CFFIPointer` to `r_variant`.
+	 *
+	 * @return True.
+	 */
 	bool data_to_variant(const uint8_t *ptr, Variant& r_variant) const override;
+	/**
+	 * Write the Variant `value` into `buffer`.
+	 *
+	 * Only PackedByteArray and CFFIPointers are supported.
+	 *
+	 * @return True if the conversion succeeded, false otherwise.
+	 */
 	bool variant_to_data(const Variant& value, uint8_t *buffer) const override;
 
 protected:
