960eccaf09fadbe9d5192e0abd67f3b50309e365
gilzoide <gilzoide@gmail.com>
2024-06-02 15:16:06 -0300

    Move struct definition in Test.gd before the functions

--- test/Test.gd
+++ test/Test.gd
@@ -1,17 +1,17 @@
 extends Label
 
 static var NativePlugin: CFFILibraryHandle = load("res://native_plugin/native_plugin.ffilibrary").open()
-static var get_answer = NativePlugin.get_function("get_answer", "int")
-static var double_int = NativePlugin.get_function("double_int", "int", ["int"])
-static var double_float = NativePlugin.get_function("double_float", "float", ["float"])
-static var get_message = NativePlugin.get_function("get_message", "const char *")
 static var ExampleStruct = NativePlugin.define_struct("ExampleStruct", {
 	"a": "int",
 	"b": "int",
 })
+static var get_answer = NativePlugin.get_function("get_answer", "int")
+static var double_int = NativePlugin.get_function("double_int", "int", ["int"])
+static var double_float = NativePlugin.get_function("double_float", "float", ["float"])
+static var get_message = NativePlugin.get_function("get_message", "const char *")
 static var get_example_struct = NativePlugin.get_function("get_example_struct", "ExampleStruct")
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	print(get_answer.invoke())
 	print(double_int.invoke(31))
