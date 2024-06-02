e2a85847285272b15d8d32babb32b5ffef6ac171
gilzoide <gilzoide@gmail.com>
2024-06-02 11:07:01 -0300

    Add some docs to CFFIStructType

--- src/cffi_struct_type.hpp
+++ src/cffi_struct_type.hpp
@@ -15,15 +15,51 @@ class CFFIScope;
 class CFFIStructType : public CFFITypeTuple, public CFFIType {
 	GDCLASS(CFFIStructType, CFFIType);
 public:
+	/**
+	 * Necessary to define a Godot class.
+	 * @warning Never use this constructor.
+	 */
 	CFFIStructType();
 
+	/**
+	 * @return Type of the field named `field_name`.
+	 *         Returns `null` if the field cannot be found in the struct.
+	 */
 	Ref<CFFIType> type_of(const StringName& field_name) const;
+	/**
+	 * @return Offset of the field named `field_name`, in bytes.
+	 *         Returns `-1` if the field cannot be found in the struct.
+	 */
 	int64_t offset_of(const StringName& field_name) const;
 
+	/**
+	 * Serializes the struct data read from `ptr` into a Dictionary.
+	 *
+	 * The returned dictionary's keys use the same name as the struct field names.
+	 *
+	 * @warning `ptr` must point to an instance of this struct type.
+	 */
 	Dictionary get_dictionary_from_struct_data(const uint8_t *ptr) const;
-	void dictionary_to_data(const Dictionary& value, uint8_t *buffer) const;
+	/**
+	 * Serializes `dict` into `buffer`, using only the values matching the struct's field names.
+	 *
+	 * Fields that are not present in the dictionary will be zero-initialized.
+	 *
+	 * @warning `buffer` must point to an instance of this struct type.
+	 */
+	void dictionary_to_data(const Dictionary& dict, uint8_t *buffer) const;
 
-	bool data_to_variant(const uint8_t *ptr, Variant& r_variant) const override;
+	/**
+	 * Write a `CFFIValue` with a shallow copy of this struct's data into `r_variant`.
+	 *
+	 * @return True.
+	 */
+	bool data_to_variant(const uint8_t *data, Variant& r_variant) const override;
+	/**
+	 * Write the Variant `value` into `buffer`.
+	 *
+	 * @return True if the conversion succeeded, false otherwise.
+	 */
 	bool variant_to_data(const Variant& value, uint8_t *buffer) const override;
 
 	static Ref<CFFIStructType> from_dictionary(const String& name, const Dictionary& fields, CFFIScope *type_scope);
