e90c0fff3c9f0a0182bdbc27eadfe2e52f2c4e52
gilzoide <gilzoide@gmail.com>
2025-07-10 22:14:55 -0300

    Fix support for passing `null` to any pointer argument

--- src/cffi_pointer_type.cpp
+++ src/cffi_pointer_type.cpp
@@ -22,18 +22,22 @@ bool CFFIPointerType::data_to_variant(const uint8_t *ptr, Variant& r_variant) co
 
 bool CFFIPointerType::variant_to_data(const Variant& value, uint8_t *buffer) const {
 	switch (value.get_type()) {
+		case Variant::Type::NIL:
+			*(const uint8_t **) buffer = nullptr;
+			return true;
+
 		case Variant::Type::PACKED_BYTE_ARRAY: {
 			PackedByteArray bytes = value;
 			*(const uint8_t **) buffer = bytes.ptr();
 			return true;
 		}
 
-		case Variant::Type::OBJECT: {
+		case Variant::Type::OBJECT:
 			if (auto pointer_value = Object::cast_to<CFFIPointer>(value)) {
 				*(uint8_t **) buffer = pointer_value->address_offset_by(0);
 				return true;
 			}
-		}
+			break;
 
 		default:
 			break;
