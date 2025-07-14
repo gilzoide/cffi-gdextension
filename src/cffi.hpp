fb19d99a2bfeda3c45dc8722b3029ee67d72ff4d
gilzoide <gilzoide@gmail.com>
2025-07-14 19:56:05 -0300

    Add methods to create null-terminated buffers from strings in CFFI

--- src/cffi.hpp
+++ src/cffi.hpp
@@ -23,6 +23,12 @@ public:
 	 */
 	Ref<CFFILibraryHandle> open(const String& name_or_path) const;
 
+	static PackedByteArray null_terminated_ascii_buffer(const String& str);
+	static PackedByteArray null_terminated_utf8_buffer(const String& str);
+	static PackedByteArray null_terminated_utf16_buffer(const String& str);
+	static PackedByteArray null_terminated_utf32_buffer(const String& str);
+	static PackedByteArray null_terminated_wchar_buffer(const String& str);
+
 	static CFFI *get_singleton();
 	static CFFI *get_or_create_singleton();
 	static void delete_singleton();
