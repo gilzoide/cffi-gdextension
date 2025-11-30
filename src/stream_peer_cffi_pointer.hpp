80d3ef8f0be840eb1bb4d056b6c7b08b8d4d1ff3
gilzoide <gilzoide@gmail.com>
2025-11-30 10:20:50 -0300

    Add StreamPeerCFFIPointer class

---
+++ src/stream_peer_cffi_pointer.hpp
@@ -0,0 +1,43 @@
+#ifndef __CFFI_POINTER_STREAM_PEER_HPP__
+#define __CFFI_POINTER_STREAM_PEER_HPP__
+
+#include <godot_cpp/classes/stream_peer_extension.hpp>
+
+#include "cffi_pointer.hpp"
+
+using namespace godot;
+
+namespace cffi {
+
+class StreamPeerCFFIPointer : public StreamPeerExtension {
+	GDCLASS(StreamPeerCFFIPointer, StreamPeerExtension)
+public:
+	Error _get_data(uint8_t *r_buffer, int32_t r_bytes, int32_t *r_received) override;
+	Error _get_partial_data(uint8_t *r_buffer, int32_t r_bytes, int32_t *r_received) override;
+	Error _put_data(const uint8_t *p_data, int32_t p_bytes, int32_t *r_sent) override;
+	Error _put_partial_data(const uint8_t *p_data, int32_t p_bytes, int32_t *r_sent) override;
+	int32_t _get_available_bytes() const override;
+
+	void set_pointer(Ref<CFFIPointer> pointer);
+	Ref<CFFIPointer> get_pointer() const;
+
+	void set_size(int size);
+	int get_size() const;
+
+	void seek(int p_pos);
+	int get_position() const;
+
+	void clear();
+
+protected:
+	static void _bind_methods();
+
+private:
+	Ref<CFFIPointer> pointer = nullptr;
+	int size = 0;
+	int cursor = 0;
+};
+
+}
+
+#endif  // __CFFI_POINTER_STREAM_PEER_HPP__
